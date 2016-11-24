9a2ca2d90a397f40f24091c912031c2e2ba50a9a
Alek Fleischer <thecrazyal02@gmail.com>
2016-11-24 14:17:32 -0800

    Initial check-in

---
+++ hooks/love_draw_end.lua
@@ -0,0 +1,5 @@
+if successfulDoink == true then
+	love.graphics.setColor(255, 255, 0, 255)
+	love.graphics.print("Regex validated!", 8, love.graphics.getHeight()-16)
+	love.graphics.setColor(255, 255, 255, 255)
+end
---
+++ info.lua
@@ -0,0 +1,11 @@
+t = ...
+
+t.shortname = "Auto Tele"
+t.longname = "Auto Teleport"
+t.author = "crazya"
+t.version = "1.0"
+t.minimumved = "b14"
+t.description = [[
+beep
+]]
+t.overrideinfo = false
