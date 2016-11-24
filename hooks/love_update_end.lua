23b50261ef4b9d5a736d85c4166a441e19e90680
Alek Fleischer <thecrazyal02@gmail.com>
2016-11-24 15:05:28 -0800

    Regex is a bit more versatile

--- hooks/love_update_end.lua
+++ hooks/love_update_end.lua
@@ -1,5 +1,5 @@
 if state == 1 then for _,v in pairs(scriptnames) do
-	if string.match(v, 't%d+%_%d+') ~= nil then
+	if string.match(v, '^t%d+[%_%-%,%s]%d+$') ~= nil then
 		successfulDoink = true;
 	end
 end end
