d69aca6f9b870ef8a24eb3dafb7a2b5f47cc2cc3
Alek Fleischer <thecrazyal02@gmail.com>
2016-11-25 08:04:13 -0800

    Comment it better

--- hooks/love_update_end.lua
+++ hooks/love_update_end.lua
@@ -1,6 +1,6 @@
 if state == 1 then for _,v in pairs(scriptnames) do
-	if string.match(v, '^t%d+%D%d+$') ~= nil then
-		--Grab the coordinates from the string
+	if string.match(v, '^t%d+%D%d+$') ~= nil then --Is this a teleport script?
+		--It is! Grab the coordinates from the string.
 		telex = tonumber(string.sub(string.match(v, 't%d+%D'), 2, -2))
 		teley = tonumber(string.sub(string.match(v, '%D%d+$'), 2, -1))
 
