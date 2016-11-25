b6220b9d8f073e852b251ef0cd77b597c4c9e6ac
Alek Fleischer <thecrazyal02@gmail.com>
2016-11-25 10:43:44 -0800

    Write to script

--- hooks/love_draw_end.lua
+++ hooks/love_draw_end.lua
@@ -1,8 +1,6 @@
 if successfulDoink == true then
 	love.graphics.setColor(255, 255, 0, 255)
-	if emptyScript == true then
-		love.graphics.print("Script is empty", 8, love.graphics.getHeight()-16)
-	elseif telex == nil or teley == nil then
+	if telex == nil or teley == nil then
 		love.graphics.print("Regex validated! Coordinates nil...", 8, love.graphics.getHeight()-16)
 	else
 		love.graphics.print("Regex validated! (" .. telex .. "," .. teley .. ")", 8, love.graphics.getHeight()-16)
--- hooks/love_update_end.lua
+++ hooks/love_update_end.lua
@@ -4,6 +4,7 @@ if state == 1 then for _,v in pairs(scriptnames) do
 		telex = tonumber(string.sub(string.match(v, 't%d+%D'), 2, -2))
 		teley = tonumber(string.sub(string.match(v, '%D%d+$'), 2, -1))
 
+		--Display debug text
 		successfulDoink = true;
 
 		--Load the script without opening the script editor
@@ -11,7 +12,13 @@ if state == 1 then for _,v in pairs(scriptnames) do
 		scriptlines = table.copy(scripts[scriptname])
 		processflaglabels()
 
-		if scriptlines[1] == "" and scriptlines[2] == nil then emptyScript = true;
-		else emptyScript = false; end
+		--Stop if script is empty
+		if not (scriptlines[1] == "" and scriptlines[2] == nil) then break end
+
+		--Write script lines
+		scriptlines[1] = "test"
+
+		--Save the script back to the level data
+		scripts[scriptname] = table.copy(scriptlines)
 	end
 end end
