f01f5d1a374ea4b375a0f120cc1915527a71cb4e
Alek Fleischer <thecrazyal02@gmail.com>
2016-11-25 12:38:59 -0800

    Fix bug where only one script is populated

--- hooks/love_update_end.lua
+++ hooks/love_update_end.lua
@@ -1,24 +1,22 @@
 if state == 1 then for _,v in pairs(scriptnames) do
 	if string.match(v, '^t%d+%D%d+$') ~= nil then --Is this a teleport script?
-		--It is! Grab the coordinates from the string.
-		telex = tonumber(string.sub(string.match(v, 't%d+%D'), 2, -2))
-		teley = tonumber(string.sub(string.match(v, '%D%d+$'), 2, -1))
-
-		--Display debug text
-		successfulDoink = true;
-
-		--Load the script without opening the script editor
+		--Yep! Load the script without opening the script editor
 		scriptname = v
 		scriptlines = table.copy(scripts[scriptname])
 		processflaglabels()
+		if scriptlines[1] == "" and scriptlines[2] == nil then --Is it empty?
+			--It is! Grab the coordinates from the string.
+			telex = tonumber(string.sub(string.match(v, 't%d+%D'), 2, -2))
+			teley = tonumber(string.sub(string.match(v, '%D%d+$'), 2, -1))
 
-		--Stop if script is empty
-		if not (scriptlines[1] == "" and scriptlines[2] == nil) then break end
+			--Display debug text
+			successfulDoink = true;
 
-		--Write script lines
-		scriptlines[1] = "test"
+			--Write script lines
+			scriptlines[1] = "test"
 
-		--Save the script back to the level data
-		scripts[scriptname] = table.copy(scriptlines)
+			--Save the script back to the level data
+			scripts[scriptname] = table.copy(scriptlines)
+		end
 	end
 end end
