a984c692993e374b8f6d340a879d0fdd47f68401
Alek Fleischer <thecrazyal02@gmail.com>
2016-11-25 16:02:30 -0800

    Add info

--- info.lua
+++ info.lua
@@ -6,6 +6,21 @@ t.author = "crazya"
 t.version = "1.0"
 t.minimumved = "b14"
 t.description = [[
-beep
+Generate scripts to teleport the player!
+When naming a script, use this format:
+
+tx,y\y
+
+The plugin will then generate a load script by that
+name and an internal script that will teleport
+the player to those coordinates.
+
+Any non-digit character can be used as a delimiter.
+Example:
+
+t4-16\y
+
+Note that coordinates start at 1 like in the editor,
+not at 0 like in internal scripting.
 ]]
 t.overrideinfo = false
